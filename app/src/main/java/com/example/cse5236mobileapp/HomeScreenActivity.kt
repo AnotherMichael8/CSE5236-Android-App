b10eaabf8eee24fa4c20fc1daa587d280f5b736d
Jake Yablok <yablok.2@buckeyemail.osu.edu>
2024-10-03 23:53:39 -0400

    Added home screen functionality

--- app/src/main/java/com/example/cse5236mobileapp/HomeScreenActivity.kt
+++ app/src/main/java/com/example/cse5236mobileapp/HomeScreenActivity.kt
@@ -8,21 +8,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
+class HomeScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_home_screen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val button_continue = findViewById<Button>(R.id.btnLogIn)
+        val button_continue = findViewById<Button>(R.id.LogoutButton)
 
         button_continue.setOnClickListener{
-            val intent = Intent(this, HomeScreenActivity::class.java)
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
     }
