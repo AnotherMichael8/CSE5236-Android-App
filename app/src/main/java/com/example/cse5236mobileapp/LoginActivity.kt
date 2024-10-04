32320dfa156394c5fec341c2aae13fda3a6313a1
Jake Yablok <yablok.2@buckeyemail.osu.edu>
2024-10-03 22:42:05 -0400

    Updated Login Page

--- app/src/main/java/com/example/cse5236mobileapp/LoginActivity.kt
+++ app/src/main/java/com/example/cse5236mobileapp/LoginActivity.kt
@@ -1,29 +1,20 @@
 package com.example.cse5236mobileapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val button_continue = findViewById<Button>(R.id.btnContinue)
-
-        button_continue.setOnClickListener{
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
         }
     }
 }
