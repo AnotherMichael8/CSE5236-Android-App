89ccf162e0bd87c737f793f91bc7dd0d2c24b273
Jake Yablok <yablok.2@buckeyemail.osu.edu>
2024-10-15 00:11:35 -0400

    Added google-services file for using Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    // Import the BoM for the Firebase platform
+    implementation(platform(libs.firebase.bom))
+
+    // Add the dependency for the Cloud Storage library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation(libs.firebase.storage)
 }
