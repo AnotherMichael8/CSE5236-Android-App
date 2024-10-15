a30fcf50fa43d7a4404cb0b43a9bd9e715ecd64d
Jake Yablok <yablok.2@buckeyemail.osu.edu>
2024-10-15 17:14:07 -0400

    Able to create new accounts from the create account page

--- app/src/main/java/com/example/cse5236mobileapp/CreateAccountFragment.kt
+++ app/src/main/java/com/example/cse5236mobileapp/CreateAccountFragment.kt
@@ -1,6 +1,7 @@
 package com.example.cse5236mobileapp
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,9 @@ import android.widget.Button
 import android.util.Log;
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,12 +25,17 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class CreateAccountFragment : Fragment(R.layout.fragment_create_account) {
+
+
     companion object {
         private const val TAG = "Create Account Fragment"
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Creating firebase reference
+        var auth = Firebase.auth
 
         // Getting view elements here
         val backButton = view.findViewById<Button>(R.id.btnBack)
@@ -46,11 +55,56 @@ class CreateAccountFragment : Fragment(R.layout.fragment_create_account) {
             var password = createPassword.text.toString()
             var reentered = reenterPassword.text.toString()
 
-            // TODO: This toast was made just for testing
-            Toast.makeText(requireContext(), username, Toast.LENGTH_LONG).show()
+
+            // See if fields are valid
+            if (checkUserInfo(username, password, reentered)) {
+                auth.createUserWithEmailAndPassword(username, password)
+                    .addOnCompleteListener(requireActivity()) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success")
+                            val user = auth.currentUser
+                            // TODO: updateUI(user)
+                            Toast.makeText(requireContext(), "Success", Toast.LENGTH_LONG).show()
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                            Toast.makeText(
+                                requireContext(),
+                                "Authentication failed.",
+                                Toast.LENGTH_SHORT,
+                            ).show()
+                            // TODO: updateUI(null)
+                        }
+                    }
+            }
 
 
         }
+    }
+
+    fun checkUserInfo(username: String, password: String, reentered: String): Boolean {
+        // Checking the three fields to see if they're empty
+        if (TextUtils.isEmpty(username)) {
+            Toast.makeText(requireContext(), "Missing Username", Toast.LENGTH_LONG).show()
+            return false
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(requireContext(), "Missing Password", Toast.LENGTH_LONG).show()
+            return false
+        }
+        if (TextUtils.isEmpty(reentered)) {
+            Toast.makeText(requireContext(), "Please Reenter Password", Toast.LENGTH_LONG).show()
+            return false
+        }
+
+        // See if passwords match
+        if(!TextUtils.equals(password,reentered)) {
+            Toast.makeText(requireContext(), "Passwords do not match", Toast.LENGTH_LONG).show()
+            return false
+        }
+
+        return true
     }
 
 
