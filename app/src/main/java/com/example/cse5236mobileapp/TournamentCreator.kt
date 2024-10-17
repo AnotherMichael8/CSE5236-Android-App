dc91f11f8ebb84a6668ad3b1203de0a295ba0e51
AnotherMichael8 <130487522+AnotherMichael8@users.noreply.github.com>
2024-10-17 01:02:09 -0400

    update

---
+++ app/src/main/java/com/example/cse5236mobileapp/TournamentCreator.kt
@@ -0,0 +1,20 @@
+package com.example.cse5236mobileapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TournamentCreator : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tournament_creator)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
