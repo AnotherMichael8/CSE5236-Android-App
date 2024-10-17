4999afd90d0a9ee9c47c4f3a124d9b0f8ae18d7b
agtivi <95649224+agtivi@users.noreply.github.com>
2024-10-17 19:42:11 -0400

    Bugged Setup of Data Retrieval
    Also made it so can't click on view behind ViewTournamentsFragment

--- app/src/main/java/com/example/cse5236mobileapp/ViewTournamentsFragment.kt
+++ app/src/main/java/com/example/cse5236mobileapp/ViewTournamentsFragment.kt
@@ -10,23 +10,37 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.cse5236mobileapp.AccountSettingsFragment.Companion
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ViewTournamentsFragment : Fragment(R.layout.fragment_view_tournaments) {
     companion object {
         private const val TAG = "View Tournaments Fragment"
     }
 
-
+    var todayTournaments = arrayListOf<String>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
 
         val previousTournaments = arrayOf("Previous Tournament 1", "Previous Tournament 2", "Previous Tournament 3", "Previous Tournament 4")
-        val todayTournaments = arrayOf("Tournament 1", "Tournament 2", "Tournament 3", "Tournament 4")
         val futureTournaments = arrayOf("Future Tournament 1", "Future Tournament 2", "Future Tournament 3", "Future Tournament 4")
 
         val tournamentContainer: LinearLayout = view.findViewById(R.id.tournamentContainer)
+        val database = FirebaseFirestore.getInstance()
+
+        val docRef = database.collection("Tournaments").document("ID for Tournaments (Template")
+        docRef.get().addOnSuccessListener { document ->
+            if (document != null) {
+                Log.d(TAG, "DocumentSnapshot data: ${document.data}")
+                todayTournaments.add(document.data?.get(key = "TournamentName").toString())
+            } else {
+                Log.d(TAG, "No such document")
+            }
+        }
+        .addOnFailureListener { exception ->
+            Log.d(TAG, "get failed with ", exception)
+        }
 
         for (tournament in todayTournaments) {
             val tournamentView = TextView(requireContext()).apply {
