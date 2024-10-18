73afb9297f2f6b6ef1f39dd409b4288db36608ff
Jake Yablok <yablok.2@buckeyemail.osu.edu>
2024-10-18 16:34:02 -0400

    Added ability to change the score

--- app/src/main/java/com/example/cse5236mobileapp/Game.kt
+++ app/src/main/java/com/example/cse5236mobileapp/Game.kt
@@ -1,6 +1,7 @@
 package com.example.cse5236mobileapp
 
 import android.util.Log
+import com.google.android.material.color.utilities.Score
 import com.google.firebase.firestore.PropertyName
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -14,14 +15,16 @@ class Game (
 )
 {
     companion object {
-        fun createGame(game: Game) {
+        fun createGame(game: Game, onResult: (String?) -> Unit) {
             val database = Firebase.firestore
 
             val uuid = "Game" + UUID.randomUUID().toString()
             database.collection("Games").document(uuid).set(game).addOnSuccessListener { document ->
                 Log.d(null, "Game created with id: $uuid")
+                onResult(uuid)
             }.addOnFailureListener {
                 Log.w(null, "Game unable to create")
+                onResult(null)
             }
         }
 
@@ -32,6 +35,18 @@ class Game (
                 Log.d(null, "Game deleted with id: $gameId")
             }.addOnFailureListener {
                 Log.w(null, "Game not deleted")
+            }
+        }
+
+        fun editPlayerScore(gameId: String, newScore: Int, isTeamOne: Boolean) {
+            val database = Firebase.firestore
+
+            val attributeName = if (isTeamOne) "teamOneScore" else "teamTwoScore"
+
+            database.collection("Games").document(gameId).update(attributeName, newScore).addOnSuccessListener {
+                Log.d(null, "Changed $attributeName to $newScore")
+            }.addOnFailureListener {
+                Log.w(null, "Failure to change score")
             }
         }
     }
