3373329052e3d6fd05682a6ba490e2c63bd97b12
Jake Yablok <yablok.2@buckeyemail.osu.edu>
2024-10-18 15:42:55 -0400

    Added game object with create game class

---
+++ app/src/main/java/com/example/cse5236mobileapp/Game.kt
@@ -0,0 +1,28 @@
+package com.example.cse5236mobileapp
+
+import android.util.Log
+import com.google.firebase.firestore.PropertyName
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.util.UUID
+
+class Game (
+    @PropertyName("TeamOne") var teamOne: String = "",
+    @PropertyName("TeamTwo") var teamTwo: String = "",
+    @PropertyName("TeamOneScore") var teamOneScore: Int = 0,
+    @PropertyName("TeamTwoScore") var teamTwoScore: Int = 0
+)
+{
+    companion object {
+        fun createGame(game: Game) {
+            val database = Firebase.firestore
+
+            val uuid = "Game" + UUID.randomUUID().toString()
+            database.collection("Games").document(uuid).set(game).addOnSuccessListener { document ->
+                Log.d(null, "Game created with id: $uuid")
+            }.addOnFailureListener {
+                Log.w(null, "Game unable to create")
+            }
+        }
+    }
+}
