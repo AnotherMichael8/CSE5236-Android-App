4ed01cce27b860b3811b743cee269ccec7913fa0
Jake Yablok <yablok.2@buckeyemail.osu.edu>
2024-11-07 13:27:19 -0500

    Created view layout for the join tournament screen

---
+++ app/src/main/java/com/example/cse5236mobileapp/ui/fragment/JoinTournamentFragment.kt
@@ -0,0 +1,30 @@
+package com.example.cse5236mobileapp.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.cse5236mobileapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [JoinTournamentFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class JoinTournamentFragment : Fragment(R.layout.fragment_join_tournament) {
+
+    override fun onViewCreated(view: View,savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // TODO: Implementation done here
+
+    }
+
+
+
+
+    companion object {
+        final val TAG = "Join Tournament Fragment"
+    }
+}
