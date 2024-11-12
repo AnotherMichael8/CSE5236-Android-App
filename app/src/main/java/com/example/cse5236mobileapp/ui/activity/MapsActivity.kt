efd12f12473ebb51381ece0d8db31f16c18679ec
agtivi <95649224+agtivi@users.noreply.github.com>
2024-11-11 23:30:01 -0500

    Basis for Branching
    Should function

--- app/src/main/java/com/example/cse5236mobileapp/ui/activity/MapsActivity.kt
+++ app/src/main/java/com/example/cse5236mobileapp/ui/activity/MapsActivity.kt
@@ -43,8 +43,6 @@ class MapsActivity : AppCompatActivity(),
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -60,46 +58,11 @@ class MapsActivity : AppCompatActivity(),
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        PermissionUtils.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
 
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
-//            onRequestPermissionsResult(LOCATION_PERMISSION_REQUEST_CODE, )
-//            onRequestPermissionsResult()
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        } else {
-            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
-                TODO("Help")
-            }
-        }
-//        // Add a marker in Sydney and move the camera
-//        val sydney = LatLng(-34.0, 151.0)
-//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-        if (!PermissionUtils.hasPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION))) {
-            PermissionUtils.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
-            if(PermissionUtils.hasPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION))){
+        enableMyLocation()
 
-
-            } else {
-                finish()
-            }
-        } else {
-            enableMyLocation()
-        }
         googleMap.setOnMyLocationButtonClickListener(this)
         googleMap.setOnMyLocationClickListener(this)
     }
